7689661078604e1611b3d5dc9e679461f892a262
Yu Tong <tyu32@dons.usfca.edu>
2019-02-14 05:32:16 -0800

    add two navbar

--- prototype2.js
+++ prototype2.js
@@ -29,7 +29,6 @@ convertRow = function(row, index){
 }
 var DrawBarChart = function(){
   // console.log("csv obj is: " + csvObj);
-  // let day = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
   let countMin = 0;
   //Maximum number of incidents
   let countMax = 453;
@@ -43,19 +42,7 @@ var DrawBarChart = function(){
   };
     let bounds = svg.node().getBoundingClientRect();
     let plotWidth = bounds.width - margin.right - margin.left;
-    let plotHeight = bounds.height - margin.top - margin.bottom;
-    //Scalling the number of incidents as yAxis
-    // let incidentScale = d3.scaleLinear()
-    //     .domain([countMin, countMax])
-    //     .range([plotHeight, 0])
-    //     .nice();
-    //
-    // //Scalling number of days of a month as xAxis
-    // let monthScale = d3.scaleBand()
-    //     .domain(outputObj.dates.reverse()) // all letters (not using the count here)
-    //     .rangeRound([0, plotWidth])
-    //     .paddingInner(0.1); // space between bars
-
+    let plotHeight = bounds.height - margin.top - margin.bottom
     let plot = svg.select("g#plot");
 
     if (plot.size() < 1) {
@@ -86,16 +73,13 @@ var arc_generator = d3.arc()
         .outerRadius(100);
 var pieData = pie(outputObj.sum);
 var pieData1 = pie(outputObj.category);
-var arcData = d3.pie().sort(null).value(function(d){
-  return d;
-})(outputObj.sum);
+
 
 var g = svg.append("g")
       .attr("transform","translate("+margin.top+","+margin.left+")");
 
 var gs = g.selectAll(".g")
-          //.data(pieData)
-          .data(arcData)
+          .data(pieData)
           .enter()
           .append("g")
           .attr("transform","translate("+plotWidth/2+","+plotHeight/2+")")
@@ -118,10 +102,7 @@ gs.append("text")
       return "translate("+x+","+y+")";
     })
     .attr("text-anchor","middle")
-    //.text(outputObj.sum)
-    // .text(function(d){
-    //   return (d.data);
-    // })
+
     .text(function(d,i){
       return outputObj.sum[i] + "\n"+ outputObj.category[i];
     })
@@ -129,50 +110,6 @@ gs.append("text")
 
 
 };
-
-
-// var text = svg.select(".labels").selectAll("text")
-//                .data(pieData);
-//
-//            // text.enter()
-//            //     .append("text")
-//            //     .attr("dy", ".35em")
-//            //     .text(function (d) {
-//            //         return d.data.label;
-//            //     });
-//
-//            function midAngle(d) {
-//                return d.startAngle + (d.endAngle - d.startAngle) / 2;
-//            }
-//
-//            text.transition().duration(1000)
-//                .attrTween("transform", function (d) {
-//                    this._current = this._current || d;
-//                    var interpolate = d3.interpolate(this._current, d);
-//                    this._current = interpolate(0);
-//                    return function (t) {
-//                        var d2 = interpolate(t);
-//                        //获取文本内容在外圆的中心位置坐标
-//                        var pos = outerArc.centroid(d2);
-//                        //然后将文本内容进行左右平移，移动到固定的radius长度，方向由饼图中心角度是否大于180度决定。
-//                        pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
-//                        return "translate(" + pos + ")";
-//                    };
-//                })
-//                .styleTween("text-anchor", function (d) {
-//                    this._current = this._current || d;
-//                    var interpolate = d3.interpolate(this._current, d);
-//                    this._current = interpolate(0);
-//                    return function (t) {
-//                        var d2 = interpolate(t);
-//                        //判断文本的锚点位置
-//                        return midAngle(d2) < Math.PI ? "start" : "end";
-//                    };
-//                });
-//
-//            text.exit()
-//                .remove();
-//              };
 
 LoadingData();
 // DrawBarChart();
